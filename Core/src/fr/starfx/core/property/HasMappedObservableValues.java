7faffdc3f529b4bf0ad3a114955231e348beb768
Luka LE ROUX <lerouxlu@ensieta.ecole>
2017-04-24 15:26:14 +0200

    First attempt at a rating framework

--- Core/src/fr/starfx/core/property/HasMappedObservableValues.java
+++ Core/src/fr/starfx/core/property/HasMappedObservableValues.java
@@ -2,6 +2,8 @@ package fr.starfx.core.property;
 
 import javafx.beans.Observable;
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.ReadOnlyDoubleProperty;
+import javafx.beans.property.ReadOnlyDoubleWrapper;
 import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.value.ObservableValue;
@@ -24,6 +26,20 @@ public interface HasMappedObservableValues {
         ReadOnlyObjectProperty<T> result = (ReadOnlyObjectProperty<T>) getObservableValueMap().get(propertyName);
         if (result == null) {
             final ReadOnlyObjectWrapper<T> wrapper = new ReadOnlyObjectWrapper<>(this, propertyName);
+            wrapper.bind(Bindings.createObjectBinding(valueSupplier, dependencies));
+            result = wrapper.getReadOnlyProperty();
+            getObservableValueMap().put(propertyName, result);
+        }
+        return result;
+    }
+
+    default ReadOnlyDoubleProperty getReadOnlyDoubleProperty(
+            String propertyName,
+            Callable<Double> valueSupplier,
+            Observable... dependencies) {
+        ReadOnlyDoubleProperty result = (ReadOnlyDoubleProperty) getObservableValueMap().get(propertyName);
+        if (result == null) {
+            final ReadOnlyDoubleWrapper wrapper = new ReadOnlyDoubleWrapper(this, propertyName);
             wrapper.bind(Bindings.createObjectBinding(valueSupplier, dependencies));
             result = wrapper.getReadOnlyProperty();
             getObservableValueMap().put(propertyName, result);
