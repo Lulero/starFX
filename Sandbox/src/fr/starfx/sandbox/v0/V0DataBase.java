78a43de6a4f1549e943e06ec0b55410bf07a4110
Luka LE ROUX <lerouxlu@ensieta.ecole>
2017-05-11 08:14:54 +0200

    refactor refactor and more refactor

--- Sandbox/src/fr/starfx/sandbox/v0/V0DataBase.java
+++ Sandbox/src/fr/starfx/sandbox/v0/V0DataBase.java
@@ -2,19 +2,22 @@ package fr.starfx.sandbox.v0;
 
 import fr.starfx.sandbox.data.Archetype;
 import fr.starfx.sandbox.data.DataBase;
-import fr.starfx.sandbox.data.Faction;
+import fr.starfx.sandbox.data.Prefix;
+import fr.starfx.sandbox.data.Suffix;
 
 public class V0DataBase extends DataBase {
 
     public V0DataBase() {
         super("V0");
-        getFactionList().addAll(
-                new Faction("Primal"),  // Improved base stats      (depends on archetype)
-                new Faction("Neo"),     // Improved skill           (depends on archetype)
-                new Faction("Blood"),   // Stronger against Fire
-                new Faction("Bone"),    // Stronger against Ash
-                new Faction("Fire"),    // Stronger against Bone
-                new Faction("Ash")      // Stronger against Blood
+        getPrefixList().addAll(
+                new Prefix("Primal"),  // Improved power
+                new Prefix("Neo")      // Improved spender
+        );
+        getSuffixList().addAll(
+                new Suffix("Blood"),   // Stronger against Fire
+                new Suffix("Bones"),    // Stronger against Ash
+                new Suffix("Fire"),    // Stronger against Bone
+                new Suffix("Ashes")      // Stronger against Blood
         );
         getArchetypeList().addAll(
                 new Archetype("Behemoth"),          // Tank, front-liner
